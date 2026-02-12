a46f90eeed6324fdc766fbbadb1c53da54bbee59
josephvijay2129 <josephvijay2129@gmail.com>
2026-02-12 07:44:51 +0000

    username or email , password there the is login data

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -147,20 +147,21 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/auth/login
 // @access  Public
 const authUser = async (req, res) => {
-    const { email, password } = req.body;
+    const { login, password } = req.body;
 
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ $or: [{ email: login }, { username: login }] });
 
     if (user && (await user.matchPassword(password))) {
         res.json({
             _id: user._id,
             name: user.name,
             email: user.email,
+            username: user.username,
             role: user.role,
             token: generateToken(user._id),
         });
     } else {
-        res.status(401).json({ message: 'Invalid email or password' });
+        res.status(401).json({ message: 'Invalid credentials' });
     }
 };
 
